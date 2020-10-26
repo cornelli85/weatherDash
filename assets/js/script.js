075db4ad6c57a0dd62b03e60cc6df11bf5ade7b3
Caesar <61262789+cornelli85@users.noreply.github.com>
2020-10-25 22:37:41 -0400

    added more functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,21 @@ $(document).ready(function () {
 
         var city = $("#search-input").val();
 
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b9058984a3809b28dc8fe823591d7860";
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b9058984a3809b28dc8fe823591d7860";
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
 
+            var cityName = response.name;
+            var temp = response.main.temp;
+            var humidity = response.main.humidity;
+            var windSpeed = response.wind.speed;
+            
             console.log(response);
+
+            $("#city-info").append($("<p>"(cityName)));
         });
 
     })
