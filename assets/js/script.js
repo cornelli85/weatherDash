246e18d40f643214a25ffd5c76e97f5e7a16573e
Caesar <61262789+cornelli85@users.noreply.github.com>
2020-11-01 21:42:26 -0500

    resolved server bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
         var city = $("#search-input").val();
 
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b9058984a3809b28dc8fe823591d7860";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b9058984a3809b28dc8fe823591d7860";
 
         $.ajax({
             url: queryURL,
